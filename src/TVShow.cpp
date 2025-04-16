152aba90e1e34355006c3dd6c676b264a7b1b68f
angelicahanley <hanleyangelica@ufl.edu>
2025-04-16 17:07:51 -0400

    Parsing file code

--- src/TVShow.cpp
+++ src/TVShow.cpp
@@ -9,7 +9,14 @@ using namespace std;
 
 
 int main(){
-    ifstream file("TMDB_tv_dataset_v3");
+
+    ifstream file("../src/TMDB_tv_dataset_v3.csv");
+
+    if(!file.is_open()){
+        cerr<< "Error: file couldn't be opened.\n";
+        return 1;
+    }
+
     string line;
     getline(file,line);
 
@@ -21,7 +28,18 @@ int main(){
         while(getline(ss,field,',')){
            fields.push_back(field);
         }
-    }
 
+        if(fields.size()>21){
+            string title=fields[1];
+            string genres = fields[20];
+            string creators = fields[21];
+
+            cout << "Title: " << title << "\n";
+            cout << "Genres: " << genres << "\n";
+            cout << "Created By: " << creators << "\n";
+            cout << "-----------------------\n";
+        }
+    }
+file.close();
     return 0;
 };
