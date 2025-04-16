721ff67146b8c216df63da0d969d979794d54f3c
angelicahanley <hanleyangelica@ufl.edu>
2025-04-16 16:52:21 -0400

    added cmake

--- src/TVShow.cpp
+++ src/TVShow.cpp
@@ -6,4 +6,22 @@
 
 using namespace std;
 
-void main();
+
+
+int main(){
+    ifstream file("TMDB_tv_dataset_v3");
+    string line;
+    getline(file,line);
+
+    while(getline(file,line)){
+        stringstream ss(line);
+        string field;
+        vector<string> fields;
+
+        while(getline(ss,field,',')){
+           fields.push_back(field);
+        }
+    }
+
+    return 0;
+};
