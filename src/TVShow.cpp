6c94cd8c6a25a713b09069e3eab93fb9897b32f4
mikaco04 <166658299+mikaco04@users.noreply.github.com>
2025-04-19 02:33:46 -0400

    just some basic functionality and trying to fix parsing

--- src/TVShow.cpp
+++ src/TVShow.cpp
@@ -4,71 +4,220 @@
 #include <vector>
 #include <string>
 #include "TVShow.h"
+#include <limits>
+#include <cctype>
+#include <algorithm>
 
 using namespace std;
 
-std::vector<std::string> parseCSV(const std::string& line) {
-    vector<string> result;
-    string category;
-    bool quoted = false;
 
-    for (int i = 0; i < line.length(); i++) {
-        if (line[i] == '"') {
-            quoted = !quoted;
-        }
-        else if (line[i] == ',' && quoted) {
-            result.push_back(category);
-            category.clear();
-        }
-        else {
-            category += line[i];
-        }
-    }
-
-    result.push_back(category);
-    return result;
+//constructor
+TVShow::TVShow (std::string name, std::string genres, std::string creators) {
+    this->name = name;
+    this->genres = genres;
+    this->creators = creators;
 }
 
-int main(){
+//prints information
+void TVShow::print() {
+    //prints out the information
+    std::cout << "TV SHOW INFORMATION:";
+    std::cout << "\nTITLE:" << name << std::endl;
+    std::cout <<"\nGENRES: " << genres << endl;
+    std::cout<< "\nCREATED BY:" << creators << std::endl;
+}
 
-    ifstream file("../src/TMDB_tv_dataset_v3.csv");
+bool TVShow::checkifMatch(std::string userSearch) {
+    //searching for the name of show
+    //lower case for sensitivity purposes
+    //citation: geeksforgeeks
+    transform(userSearch.begin(), userSearch.end(), userSearch.begin(), ::tolower);
+    transform(name.begin(), name.end(), name.begin(), ::tolower);
+    if (name.find(userSearch) != std::string::npos) {
+        return true;
+    }
+    return false;
+}
 
-    if(!file.is_open()){
-        cerr<< "Error: file couldn't be opened.\n";
-        return 1;
+
+/* function purpose: so user can input their search multiple ways without
+ a whitespace changing the result */
+string TVShow::trim (const string& s) {
+    const string whitespace = " \t\r\n";
+    //these are characters that can be considered a whitespace
+
+    //trying to find the first whitespace character
+    int first = s.find_first_not_of(whitespace); //citation: cplusplus
+    if (first == string::npos) { //citation: geeksforgeeks
+        //case: only whitespace, so return an empty string
+        return "";
+    }
+    //position of the last nonwhitespace char
+    int last = s.find_last_not_of(whitespace); //citation: cplusplus
+
+    //string that contains no whitespace
+    //citation: cplusplus
+    return s.substr(first, last - first + 1);
+}
+
+
+
+//checks if string is number, this was for a trailing number that kept appearing in output
+//not happening anymore but...
+bool TVShow::number(const string& s) {
+    if (s.empty()) {
+        return false; //empty -> no digit
     }
 
-    string line;
-    getline(file,line);
-
-    while(getline(file,line)){
-
-        vector<string> fields = parseCSV(line);
-        // stringstream ss(line);
-        // string field;
-        // vector<string> fields;
-        //
-        // while(getline(ss,field,',')){
-        //    fields.push_back(field);
-        // }
-        //git hub loves us
-        for (int i = 0; i < fields.size(); i++) {
-            cout << i << ": " << fields[i] << endl;
-        }
-
-        cout << "--------------------\n";
-
-        if(fields.size()>21){
-            string title=fields[1];
-            string genres = fields[20];
-            string creators = fields[21];
-
-            cout << "Title: " << title << "\n";
-            cout << "Genres: " << genres << "\n";
-            cout << "Created By: " << creators << "\n";
-            cout << "-----------------------\n";
+    for (unsigned int i = 0; i < s.length(); i++) {
+        if (!isdigit(s[i])) {
+            return false; //if we can't convert to digit, not a number
         }
     }
-file.close();
+    return true;
+}
+
+//reads a row from the file
+vector <string> TVShow::readRow(istream& input) {
+    vector <string> columns;
+    string category;
+    bool quotes = false;
+    char c;
+
+    while (input.get(c)) {
+        //handling quotes
+        if (c == '"') {
+            if (quotes && input.peek() == '"') {
+                category += '"';
+                input.get();
+            }
+            else {
+                quotes = !quotes;
+            }
+        }
+
+        //handling commas
+        else if (c == ',' && !quotes) {
+            columns.push_back(category);
+            category.clear();
+        }
+        else if (c == '\n' && !quotes) { //line ended
+            columns.push_back(category);
+            break;
+        }
+        else {
+            category += c; //add character to column
+        }
+    }
+    return columns; //columns from the row that was read
+}
+
+//gets column information
+string TVShow::column(vector<string> row, int colNum) {
+    if (colNum >= 0 && colNum < row.size()) {
+        if (!row[colNum].empty()) {
+            return row[colNum]; //if it is not empty & valid
+        }
+    }
+    return "No information available.";
+}
+
+//accessing wanted columns, not all of the ones in csv
+vector<string> TVShow::filterCol (vector<string> row, vector<int> index) {
+    vector<string> columns;
+    for (int i = 0; i < index.size(); i++) {
+        if (i >= 0 && i < row.size()) {
+            columns.push_back(row[i]);
+        }
+        else {
+            columns.push_back("no info available");
+        }
+    }
+    return columns;
+}
+
+//finding show, printing show name, genre, and who it was created by
+void TVShow::findShow(string csvFile, string userinput) {
+    ifstream file(csvFile);
+    if (!file) {
+        cerr << "Error opening file " << csvFile << endl;
+        return;
+    }
+
+    vector <string> header = readRow(file);
+
+    //assigning -1 bc we haven't found them yet
+    int colName = -1;
+    int colGenres = -1;
+    int colCreatedBy = -1;
+
+    //assigning indices based on the csv column names
+    for (int i = 0; i < header.size(); i++) {
+        transform(header[i].begin(), header[i].end(), header[i].begin(), ::tolower);
+        //citation: geeksforgeeks
+        if (header[i]== "name") {
+            colName = i;
+        }
+        if (header[i] == "genres") {
+            colGenres = i;
+        }
+        if (header[i] == "created_by") {
+            colCreatedBy = i;
+        }
+    }
+
+    if (colName <0 || colGenres < 0 || colCreatedBy < 0) {
+        cerr << "error";
+        return;
+    }
+
+    //user input trimmed and made all lowercase
+    string trimmedSearch = trim(userinput);
+    transform(trimmedSearch.begin(), trimmedSearch.end(), trimmedSearch.begin(), ::tolower);
+    //citation: geeksforgeeks
+
+    bool showFound = false;
+    while (true) {
+        vector<string> row = readRow(file);
+        if (row.empty()) {
+            break;
+        }
+
+        //trimming the categories
+        string title = trim(row[colName]);
+        string genres = trim(row[colGenres]);
+        string creators = trim(row[colCreatedBy]);
+
+        //this while statement is dealing with multiple creators and commas
+        while (row.size() > colCreatedBy+1) {
+            string followingCreator = trim(row[colCreatedBy+1]);
+            if (followingCreator.size() > 2 && !number(followingCreator)) {
+                creators += "," + followingCreator; //add that creator
+            }
+            row.erase(row.begin()+colCreatedBy+1);
+        }
+
+        TVShow show(title, genres, creators);
+
+        //if theres a match, show is found and displayed
+        if (show.checkifMatch(trimmedSearch)) {
+            show.print();
+            showFound = true;
+        }
+
+    }
+
+    if (!showFound) {
+        cout << "Sorry! No titles match :( Please try again.";
+    }
+}
+
+int main() {
+    cout << "Search for show title: " << endl;
+    string search;
+    getline(cin, search);
+
+    TVShow userShow(" ", " ", " ");
+    userShow.findShow("../src/TMDB_tv_dataset_v3.csv", search);
     return 0;
-};
+}
--- src/TVShow.h
+++ src/TVShow.h
@@ -1,13 +1,38 @@
 //
 // Created by josed on 4/16/2025.
 //
-
 #ifndef TVSHOW_H
 #define TVSHOW_H
 #include <string>
 #include <vector>
 
-std::vector<std::string> parseCSV(const std::string&);
+
+class TVShow {
+public:
+    std::string name;
+    std::string genres;
+    std:: string creators;
+
+    //constructor
+    TVShow(std::string name, std::string genres, std::string creators);
+    //prints out the information
+    void print();
+    //searching for the name of show
+    bool checkifMatch (std::string userSearch);
+    //trimming whitespaces in user input
+    std::string trim (const std::string& s);
+    //this function was to hard code the 2750 out but its not working...
+    bool number(const std::string& s);
+    //reads the row
+    std::vector <std::string> readRow(std::istream& input);
+    //gets column information
+    std::string column(std::vector<std::string> row, int colNum);
+    //there are extra columns in the database we don't want, this is to filter them out
+    std::vector<std::string> filterCol (std::vector<std::string> row, std::vector<int> index);
+    //finding show
+    void findShow(std::string csvFile, std::string userinput);
+};
+
 
 
 #endif //TVSHOW_H
